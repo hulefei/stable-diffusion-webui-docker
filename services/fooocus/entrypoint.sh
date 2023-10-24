4be3becd8a461e561dd952150a0fe28127588e18
hulefei <hulefei29@gmail.com>
2023-10-24 13:13:46 +0800

    [add]add inpaint

--- services/fooocus/entrypoint.sh
+++ services/fooocus/entrypoint.sh
@@ -5,6 +5,7 @@ echo Hello World
 # sd-webui 与 fooocus 的目录映射
 cp -vrfTs /data/models/Stable-diffusion/ "${ROOT}/models/checkpoints/"
 cp -vrfTs /data/models/Lora/ "${ROOT}/models/loras/"
+cp -vrfTs /data/models/inpaint/ "${ROOT}/models/inpaint/"
 cp -vrfTs /data/models/vae_approx/vaeapp_sd15.pt "${ROOT}/models/vae_approx/vaeapp_sd15.pth"
 cp -vrfTs /data/models/prompt_expansion/fooocus_expansion/fooocus_expansion.bin "${ROOT}/models/prompt_expansion/fooocus_expansion/pytorch_model.bin"
 
