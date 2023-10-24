15bfe705422e3d2b0a051014ac02e72767ddc04d
hulefei <hulefei29@gmail.com>
2023-10-24 10:52:39 +0800

    [add]使用 profile

--- services/fooocus/entrypoint.sh
+++ services/fooocus/entrypoint.sh
@@ -2,9 +2,9 @@
 
 echo Hello World
 
-# mkdir -p "/Fooocus/models/checkpoints/"
+# sd-webui 与 fooocus 的目录映射
 cp -vrfTs /data/models/Stable-diffusion/ "${ROOT}/models/checkpoints/"
-cp -vrfTs /data/models/loras/ "${ROOT}/models/loras/"
+cp -vrfTs /data/models/Lora/ "${ROOT}/models/loras/"
 cp -vrfTs /data/models/vae_approx/vaeapp_sd15.pt "${ROOT}/models/vae_approx/vaeapp_sd15.pth"
 cp -vrfTs /data/models/prompt_expansion/fooocus_expansion/fooocus_expansion.bin "${ROOT}/models/prompt_expansion/fooocus_expansion/pytorch_model.bin"
 
